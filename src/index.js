6dcbd31e854339530b2a64f98532890af1657f7f
Will Lutz <lutz@njit.edu>
2019-11-05 11:52:31 -0500

    static cleanup

--- src/index.js
+++ src/index.js
@@ -2,28 +2,20 @@ const fetch = require('node-fetch');
 const base64 = require('base-64');
 
 class JoanAPI {
-  constructor() {
-    this.credentials = { 
-      expires_in: 36000,
-      token_type: 'Bearer',
-      scope: 'read write'
-    };
-    this.expiry = new Date();
-  }
-
   static configure({clientId, secret}){
     JoanAPI.prototype.clientId = clientId;
     JoanAPI.prototype.secret = secret;
   }
 
-  static getToken(){
-    if (this.clientId === null || this.secret === null) throw new Error("Have not configured api credentials.")
+  static async getToken(){
+    const {clientId, secret} = JoanAPI.prototype
+    if (clientId === null || secret === null) throw new Error("Have not configured api credentials.")  
 
     return new Promise((resolve, reject) => {
       fetch("https://portal.getjoan.com/api/token/", {
         body: "grant_type=client_credentials",
         headers: {
-          "Authorization": `Basic ${base64.encode(`${this.clientId}:${this.secret}`)}`,
+          "Authorization": `Basic ${base64.encode(`${clientId}:${secret}`)}`,
           "Content-Type": "application/x-www-form-urlencoded"
         },
         method: "POST"
@@ -44,51 +36,54 @@ class JoanAPI {
     })
   }
 
-  async initialize() {
-    if (!!this.credentials.access_token || this.expiry < new Date()) {
-      await this.getToken();
+  static async initialize() {
+    const {credentials, expiry} = JoanAPI.prototype
+    if (!credentials || !credentials.access_token || expiry < new Date()) {
+      await JoanAPI.getToken();
     }
+    console.log('prototype', JoanAPI.prototype)
   }
 
   // GET Endpoints here
   static async me(){
-    return this.getReq('me')
+    return JoanAPI.getReq('me')
   }
 
   static async users(){
-    return this.getReq('users')
+    return JoanAPI.getReq('users')
   }
 
   static async events(){
-    return this.getReq('events')
+    return JoanAPI.getReq('events')
   }
 
   static async rooms(){
-    return this.getReq('rooms')
+    return JoanAPI.getReq('rooms')
   }
 
   static async devices(){
-    return this.getReq('devices')
+    return JoanAPI.getReq('devices')
   }
 
   // POST With a body
   static async book(data){
-    await this.initialize();
+    await JoanAPI.initialize();
+    const {access_token} = JoanAPI.prototype.credentials;
 
     return new Promise((resolve, reject) => {
       fetch("https://portal.getjoan.com/api/v1.0/events/book/", {
         body: JSON.stringify(data),
         headers: {
           Accept: "*/*",
-          "Authorization": `Bearer ${this.credentials.access_token}`,
+          "Authorization": `Bearer ${access_token}`,
           "Cache-Control": "no-cache",
           "Content-Type": "application/json",
         },
         method: "POST"
       })
       .then((res) => {
-        if (res.ok) { return res; } 
-        else { reject(res.statusText); }
+        if (res.ok) return res;
+        else reject(res.statusText);
       })
       .then(res => res.json())
       .then(json => resolve(json))
@@ -96,12 +91,43 @@ class JoanAPI {
     })
   }
 
-  async getReq(endpoint = 'me') {
-    await this.initialize();
+  static async cancel(data){
+    // {
+    //   "event_id": "id",
+    //   "timezone": "America/New_York",
+    //   "room_id": "room address"
+    // }
+    await JoanAPI.initialize();
+    const {access_token} = JoanAPI.prototype.credentials;
+
+    return new Promise((resolve, reject) => {
+      fetch("https://portal.getjoan.com/api/v1.0/events/cancel/", {
+        body: JSON.stringify(data),
+        headers: {
+          Accept: "*/*",
+          "Authorization": `Bearer ${access_token}`,
+          "Cache-Control": "no-cache",
+          "Content-Type": "application/json",
+        },
+        method: "POST"
+      })
+      .then((res) => {
+        if (res.ok) return res;
+        else reject(res.statusText);
+      })
+      .then(res => res.json())
+      .then(json => resolve(json))
+      .catch(err => reject(err))
+    })
+  }
+
+  static async getReq(endpoint = 'me') {
+    await JoanAPI.initialize();
+    const {access_token} = JoanAPI.prototype.credentials;
 
     return new Promise((resolve, reject) => {
       fetch(`https://portal.getjoan.com/api/v1.0/${endpoint}`, {
-        headers: { "Authorization": `Bearer ${this.credentials.access_token}`},
+        headers: { "Authorization": `Bearer ${access_token}`},
         method: 'GET'
       })
       .then((res) => {
@@ -112,7 +138,6 @@ class JoanAPI {
       .then(json => resolve(json))
       .catch(err => reject(err))
     })
-
   }
 }
 
